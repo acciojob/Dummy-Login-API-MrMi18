24128eeb56343a60bc6fe87e8dc6c1cfbeb7e021
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-07-06 11:10:49 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,77 +1,74 @@
-import React, {  useState } from 'react'
+import React, { useState } from "react";
 const userData = [
-    {
-        id: 1,
-        name: "ABC",
-        email: "abc@gmail.com",
-        password: "12"
-    },
-    {
-        id: 2,
-        name: "DEF",
-        email: "def@gmail.com",
-        password: "1234"
-    },
-    {
-        id: 3,
-        name: "GHI",
-        email: "ghi@gmail.com",
-        password: "123456"
-    }
-]
-const App = () => {
-    const [email,setEmail] = useState("");
-    const [ password , setPassword] = useState("")
-    const[errorMessage,setErrorMessage] = useState("");
-    const [id,setId] = useState("")
-    const onSubmit = (e) =>{
-        e.preventDefault()
-        const data = userData.filter(dt => dt.email === email);
-        if(data.length==0){
-            setErrorMessage("User not found")
-            setId("user-error")
-            setTimeout(() =>{
-                console.log(errorMessage);
-                
-            },3000)
-            return;
-        }
-        const pass = userData.filter(dt => dt.email === email && dt.password === password);
-        setId("password-error");
-        if(pass.length===0){
-            setErrorMessage("Password Incorrect")
-            setTimeout(() =>{
-                console.log(errorMessage);
-                
-            },3000)
-            return;
-        }
-        setTimeout(()=>{
-            console.log(data[0]);
-            setEmail("");
-            setPassword("");
-            setErrorMessage("")
-            setId("")
-        },3000)
+  {
+    id: 1,
+    name: "ABC",
+    email: "abc@gmail.com",
+    password: "12",
+  },
+  {
+    id: 2,
+    name: "DEF",
+    email: "def@gmail.com",
+    password: "1234",
+  },
+  {
+    id: 3,
+    name: "GHI",
+    email: "ghi@gmail.com",
+    password: "123456",
+  },
+];
 
-    }
+function App() {
+  const [passwordError, setPasswordError] = useState(null);
+  const [userError, setUserError] = useState(null);
+  const [inputData, setInputData] = useState({ email: "", password: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      for (let i = 0; i < userData.length; i++) {
+        if (userData[i].email === inputData.email) {
+          if (userData[i].password === inputData.password) {
+            console.log(userData[i]);
+          } else {
+            setPasswordError(true);
+          }
+          return;
+        }
+      }
+      setUserError(true);
+    }, 3000);
+  };
+
   return (
     <div>
-        <form onSubmit={onSubmit}>
-            <label htmlFor='email'>Email</label>
-            <input id='input-email' type='text' value={email} onChange={(e) => setEmail (e.target.value)}/>
-           
-            <br/>
-            <br/>
-            <label htmlFor='password'>Password</label>
-            <input id='input-password' type='password' value={password} onChange={(e) => setPassword (e.target.value)}/>
-            <br/>
-            {errorMessage &&  <p id={id} >{errorMessage}</p>}
-            <button type='submit' id='submit-form-btn' >Submit</button>
-        </form>
-
+      <form>
+        <input
+          type="email"
+          value={inputData.email}
+          onChange={(e) => {
+            const { value } = e.target;
+            setUserError("");
+            setInputData((prev) => ({ ...prev, email: value }));
+          }}
+        />
+        <input
+          type="password"
+          value={inputData.password}
+          onChange={(e) => {
+            const { value } = e.target;
+            setPasswordError("");
+            setInputData((prev) => ({ ...prev, password: value }));
+          }}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+      {passwordError && <p id="password-error">Password Incorrect</p>}
+      {userError && <p id="user-error">User not found</p>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
